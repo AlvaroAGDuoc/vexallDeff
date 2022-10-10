59bb4bcb25185f7f764f1b17993b80d6af25067d
AlvaroAGDuoc <a.aguerog@duocuc.cl>
2022-10-10 15:51:32 -0300

    mapa malo

--- src/app/pages/crear-ruta/crear-ruta.page.ts
+++ src/app/pages/crear-ruta/crear-ruta.page.ts
@@ -2,74 +2,77 @@
 
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-// import { GeolocationService } from 'src/app/services/geolocation.service';
 
 @Component({
   selector: 'app-crear-ruta',
   templateUrl: './crear-ruta.page.html',
   styleUrls: ['./crear-ruta.page.scss'],
 })
+
+
+
 export class CrearRutaPage implements OnInit {
+
+
 
   @ViewChild('divMap') divMap!: ElementRef;
   @ViewChild('rutaInicio') rutaInicio!: ElementRef;
+
 
   mapa!: google.maps.Map;
   marcadores: google.maps.Marker[];
   
   formMapas!: FormGroup;
 
+
   constructor(private renderer: Renderer2) {
     this.marcadores = [];
 
     this.formMapas = new FormGroup({
-      rutaInicio: new FormControl('')
+      rutaInicio: new FormControl(''),
+      rutaFin: new FormControl('')
     })
-    
-
   }
 
-  ngOnInit() {
-  }
+  // getPlaceAutocomplete() {
+	// 	const  autocomplete  =  new google.maps.places.Autocomplete(this.rutaInicio.nativeElement,
+	// 	{
+	// 		componentRestrictions: { country:  'CL' },
+	// 		types: ['establishment', 'geocode'] 
+	// 	});
+
+	// 	google.maps.event.addListener(autocomplete, 'place_changed', () => {
+	// 		const  place  =  autocomplete.getPlace();
+	// 		const  myLatlng  =  place.geometry.location;
+	// 		const  mapOptions  = {
+	// 			zoom:  15,
+	// 			center:  myLatlng
+	// 		};
+	// 		const  map  =  new  google.maps.Map(this.divMap.nativeElement, mapOptions);
+	// 		const  marker  =  new  google.maps.Marker({
+	// 			position:  myLatlng,
+	// 			title:  place.name
+	// 		});
+	// 		marker.setMap(map);
+	// 	});
+	// }
+ 
+
+
+
+  ngOnInit() {}
 
   ngAfterViewInit(): void {
     if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(async(position) => {
-        await this.cargarMapa(position);
-        this.cargarAutoComplete();
+        await this.cargarMapa(position);   
+
       })
     } else {
       console.log('error')
     }
   }
 
-  buscarRuta() {
-    console.log('Datos del formulario: ', this.formMapas.value)
-  }
-
-
-
-  private cargarAutoComplete() {
-    const autocomplete = new google.maps.places.Autocomplete(this.renderer.selectRootElement(this.rutaInicio.nativeElement), {
-      componentRestrictions: {
-        country: ["CL"]
-      },
-      fields: ["address_components", "geometry"],
-      types: ["address"],
-    })
-
-    google.maps.event.addListener(autocomplete, 'place_changed', () => {
-      const place: any = autocomplete.getPlace();
-      console.log('El place completo es: ', place)
-
-      this.mapa.setCenter(place.geometry.location);
-      const marker = new google.maps.Marker({
-        position: place.geometry.location
-      });
-      marker.setMap(this.mapa);
-    })
-
-  }
 
 
 
@@ -80,7 +83,6 @@ export class CrearRutaPage implements OnInit {
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 
-
     this.mapa = new google.maps.Map(this.renderer.selectRootElement(this.divMap.nativeElement), opciones)
     const markerPosition = new google.maps.Marker({
       position: this.mapa.getCenter(),
@@ -89,6 +91,9 @@ export class CrearRutaPage implements OnInit {
     markerPosition.setMap(this.mapa);
     this.marcadores.push(markerPosition);
     console.log('mapa cargado')
+    
   }
 
 }
+
+
